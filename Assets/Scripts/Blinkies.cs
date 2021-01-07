6a12eb3fcbb6a147191b5693ade4b95de1f23f06
Mega Banzo <timkern42@gmail.com>
2021-01-08 12:24:33 +1300

    added lights to circles

--- Assets/Scripts/Blinkies.cs
+++ Assets/Scripts/Blinkies.cs
@@ -28,16 +28,32 @@ public class Blinkies : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+
+        circles[0].GetComponent<Light>().color = GameMaster.colors[GameMaster.i];
+        circles[1].GetComponent<Light>().color = GameMaster.colors[GameMaster.i];
+        circles[2].GetComponent<Light>().color = GameMaster.colors[GameMaster.i];
+        circles[3].GetComponent<Light>().color = GameMaster.colors[GameMaster.i];
+
         if (timer > 4 * interval)
         {
             timer = 0;
         }
         if (timer <= interval)
         {
+
             circles[0].GetComponent<SpriteRenderer>().sprite = spriteSet[1 + (GameMaster.i * 2)];
             circles[1].GetComponent<SpriteRenderer>().sprite = spriteSet[0 + (GameMaster.i * 2)];
             circles[2].GetComponent<SpriteRenderer>().sprite = spriteSet[0 + (GameMaster.i * 2)];
             circles[3].GetComponent<SpriteRenderer>().sprite = spriteSet[0 + (GameMaster.i * 2)];
+
+            
+
+            circles[0].GetComponent<Light>().enabled = true;
+            circles[1].GetComponent<Light>().enabled = false;
+            circles[2].GetComponent<Light>().enabled = false;
+            circles[3].GetComponent<Light>().enabled = false;
+
+            
 
         }
         else if(interval < timer && timer <= interval * 2 )
@@ -46,6 +62,10 @@ public class Blinkies : MonoBehaviour
             circles[1].GetComponent<SpriteRenderer>().sprite = spriteSet[1 + (GameMaster.i * 2)];
             circles[2].GetComponent<SpriteRenderer>().sprite = spriteSet[0 + (GameMaster.i * 2)];
             circles[3].GetComponent<SpriteRenderer>().sprite = spriteSet[0 + (GameMaster.i * 2)];
+            circles[0].GetComponent<Light>().enabled = false;
+            circles[1].GetComponent<Light>().enabled = true;
+            circles[2].GetComponent<Light>().enabled = false;
+            circles[3].GetComponent<Light>().enabled = false;
         }
         else if (interval * 2 < timer && timer <= interval * 3)
         {
@@ -53,6 +73,13 @@ public class Blinkies : MonoBehaviour
             circles[1].GetComponent<SpriteRenderer>().sprite = spriteSet[0 + (GameMaster.i * 2)];
             circles[2].GetComponent<SpriteRenderer>().sprite = spriteSet[1 + (GameMaster.i * 2)];
             circles[3].GetComponent<SpriteRenderer>().sprite = spriteSet[0 + (GameMaster.i * 2)];
+            
+            circles[0].GetComponent<Light>().enabled = false;
+            circles[1].GetComponent<Light>().enabled = false;
+            circles[2].GetComponent<Light>().enabled = true;
+            circles[3].GetComponent<Light>().enabled = false;
+
+
         }
         else if (interval * 3 < timer && timer <= interval * 4)
         {
@@ -60,6 +87,11 @@ public class Blinkies : MonoBehaviour
             circles[1].GetComponent<SpriteRenderer>().sprite = spriteSet[0 + (GameMaster.i * 2)];
             circles[2].GetComponent<SpriteRenderer>().sprite = spriteSet[0 + (GameMaster.i * 2)];
             circles[3].GetComponent<SpriteRenderer>().sprite = spriteSet[1 + (GameMaster.i * 2)];
+
+            circles[0].GetComponent<Light>().enabled = false;
+            circles[1].GetComponent<Light>().enabled = false;
+            circles[2].GetComponent<Light>().enabled = false;
+            circles[3].GetComponent<Light>().enabled = true;
         }
     }
 }
--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -11,7 +11,7 @@ public class GameMaster : MonoBehaviour
     public static int score;
     public static int sectionLights = 0;
     public static int[] multi = { 1, 2, 3, 5 };
-    public static int i;
+    public static int i= 2;
     public static Color32[] colors = { new Color32(10, 10, 250, 255), new Color32(10, 230, 0, 255),
             new Color32(250, 210, 8, 255),new Color32(210, 10, 0, 255) };
     public static bool brickReset;
